484a1dd19850101f36f6d538ffafc7e809bb0192
Daniel Sodré <130774958+DanielJavaprogrammer@users.noreply.github.com>
2024-05-26 13:07:58 -0300

    ok

--- java.js
+++
@@ -1,7 +0,0 @@
-var nome = 'Daniel';
-var salario = 1200;
-var cargo = 'Gerente de compras';
-
-console.log("Nome do funcionario: "+ nome);
-console.log("Ultimo do salario: " + salario.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }));
-console.log("Cargo do Funcionario: "+ cargo);
