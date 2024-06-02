540de2b5e169129a1171766e7317d596ece7dfd6
Daniel Sodré <130774958+DanielJavaprogrammer@users.noreply.github.com>
2024-06-01 22:45:56 -0300

    feito

--- JavaScript/exerciciosJS/ex06.js
+++ JavaScript/exerciciosJS/ex06.js
@@ -1,1 +1,7 @@
 //6 - Faça um algoritmo que leia um valor qualquer e imprima na tela com um reajuste de 5%.
+
+var salario = 1000;
+var reajuste = salario * 0.10; // Calcula 5% do salário
+var salarioComReajuste = salario + reajuste; // Adiciona o reajuste ao salário
+console.log("Salário com reajuste de 5%: " + salarioComReajuste.toFixed(2));
+
---
+++ JavaScript/exerciciosJS/ex07.js
@@ -0,0 +1,11 @@
+//7 - Faça um algoritmo que leia dois valores booleanos (lógicos) e determine se ambos são VERDADEIRO ou FALSO.
+var valor1 = true;
+var valor2 = true;
+
+if (valor1 === true && valor2 === true) {
+    console.log("Ambos os valores são VERDADEIROS.");
+} else if (valor1 === false && valor2 === false) {
+    console.log("Ambos os valores são FALSOS.");
+} else {
+    console.log("Os valores são diferentes.");
+}
