a39a230f43c4f955767209b1a59810691563fef2
Daniel Sodré <130774958+DanielJavaprogrammer@users.noreply.github.com>
2024-06-02 21:23:45 -0300

    feito 08

--- JavaScript/exerciciosJS/ex08.js
+++ JavaScript/exerciciosJS/ex08.js
@@ -1,7 +1,8 @@
 // Faça um algoritmo que leia três valores inteiros diferentes e imprima na tela os valores em ordem decrescente.
 
-var valores = [12, 45, 55]
+var valores = [12, 45, 55];
 
-for (let i = 0; i < valores.length; i++){
-    let x = 0  
+// Itera sobre o array a partir do último elemento
+for (let i = valores.length - 1; i >= 0; i--) {
+    console.log(valores[i]);
 }
