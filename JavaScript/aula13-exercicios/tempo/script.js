7d6dbed50358a6cefac5917fc12cc1a59593d89b
Daniel Correia <130774958+DanielJavaprogrammer@users.noreply.github.com>
2024-06-10 16:30:03 -0300

    ok

--- JavaScript/aula13-exercicios/tempo/script.js
+++ JavaScript/aula13-exercicios/tempo/script.js
@@ -1,4 +1,19 @@
-// var hora = new Date();
-// var relogio = hora.getDay()
+var fundo = document.querySelector("body");
+function time() {
 
-var nome = window.document.querySelector(".horario").innerHTML="hello";
+    var hora = new Date();
+    var relogio = hora.getHours();
+    var minutos = hora.getMinutes();
+    var nome = document.querySelector(".horario");
+
+    if (relogio > 0 && relogio < 12) {
+        nome.innerHTML = `Agora são ${relogio}:${minutos} hora(as). Bom dia.`;
+        fundo.style.backgroundColor = "#fffdcd"
+    } else if (relogio >= 12 && relogio < 19) {
+        nome.innerHTML = `Agora são ${relogio}:${minutos} hora(as). Boa tarde.`;
+        fundo.style.backgroundColor = "#f0ab50"
+    } else {
+        nome.innerHTML = `Agora são ${relogio} hora(s)`
+        fundo.style.backgroundColor = "rgb(34,34,34)"
+    }
+}
