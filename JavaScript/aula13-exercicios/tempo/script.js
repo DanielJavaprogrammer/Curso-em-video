55b1bfeb646f956ecb7009711c2dcbde204bb6ad
Daniel Sodré <130774958+DanielJavaprogrammer@users.noreply.github.com>
2024-06-10 21:51:41 -0300

    projeto relogio
    Projeto relogio finalizado.

--- JavaScript/aula13-exercicios/tempo/script.js
+++ JavaScript/aula13-exercicios/tempo/script.js
@@ -1,19 +1,26 @@
 var fundo = document.querySelector("body");
-function time() {
+
+function timeup() {
 
     var hora = new Date();
     var relogio = hora.getHours();
     var minutos = hora.getMinutes();
     var nome = document.querySelector(".horario");
+    var img = document.querySelector(".tempo-foto img")
+    var doisPontos = ":"
 
     if (relogio > 0 && relogio < 12) {
-        nome.innerHTML = `Agora são ${relogio}:${minutos} hora(as). Bom dia.`;
+        nome.innerHTML = `Agora são ${relogio}<span class="piscar">:</span>${minutos} hora(s)`;
         fundo.style.backgroundColor = "#fffdcd"
     } else if (relogio >= 12 && relogio < 19) {
-        nome.innerHTML = `Agora são ${relogio}:${minutos} hora(as). Boa tarde.`;
+        nome.innerHTML = `Agora são ${relogio}<span class="piscar">:</span>${minutos} hora(s)`;
         fundo.style.backgroundColor = "#f0ab50"
+        img.src = "afternoon.jpg"
+
     } else {
-        nome.innerHTML = `Agora são ${relogio} hora(s)`
+        nome.innerHTML = `Agora são ${relogio}<span class="piscar">:</span>${minutos} hora(s)`;
         fundo.style.backgroundColor = "rgb(34,34,34)"
+        img.src = "night.jpg"
     }
 }
+setInterval(timeup, 1000);
