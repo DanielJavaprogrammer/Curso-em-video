6f8738200e6c6cbebb7b18ef63cacfa78a8178cc
Daniel Correia <130774958+DanielJavaprogrammer@users.noreply.github.com>
2024-07-22 16:53:11 -0300

    ok

---
+++ JavaScript/aula 14/for.js
@@ -0,0 +1,5 @@
+var c = 1;
+
+for (var c = 1; a < c; a++) {
+    console.log(c);
+}
---
+++ JavaScript/aula13-exercicios/laco-while.js
@@ -0,0 +1,16 @@
+var a = 1;
+
+// ou Faz o bloco e testa Antes
+
+while (a <= 6) {
+    console.log("Bom dia!");
+    a++;
+}
+
+// ou Faz o bloco e testa depois
+
+
+do {
+    console.log("Bom dia!");
+    a++;
+} while (a <= 6)
