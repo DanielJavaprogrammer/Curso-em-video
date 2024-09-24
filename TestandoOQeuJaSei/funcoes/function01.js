0753a1010806151d0480ac87d99ae61f18d2b059
Daniel Correia <130774958+DanielJavaprogrammer@users.noreply.github.com>
2024-09-24 14:49:48 -0300

    ok

--- TestandoOQeuJaSei/funcoes/function01.js
+++ TestandoOQeuJaSei/funcoes/function01.js
@@ -5,23 +5,23 @@ let dia = data.getDate();
 let mes = data.getMonth();
 let ano = data.getFullYear();
 
-// const horaAtual = () => {
-//     return ("Agora são " + hora + " e " + minuto + " minutos.");
-// }
-// console.log(horaAtual());
+const horaAtual = () => {
+    return ("Agora são " + hora + " e " + minuto + " minutos.");
+}
+console.log(horaAtual());
 
-// // Função para ver nome do funcionario. (Usando parametro).
-// const funcionario = (nome) => {
-//     return ("Bem vindo " + nome + "!");
-// }
-// console.log(funcionario("Lucas"));
+// Função para ver nome do funcionario. (Usando parametro).
+const funcionario = (nome) => {
+    return ("Bem vindo " + nome + "!");
+}
+console.log(funcionario("Lucas"));
 
-// // Funcção que calcula area (com parametro).
-// const calcularArea = (largura, altura) => {
-//     return largura * altura;
-// }
+// Funcção que calcula area (com parametro).
+const calcularArea = (largura, altura) => {
+    return largura * altura;
+}
 
-// console.log("A area completa é " + calcularArea(5, 10));
+console.log("A area completa é " + calcularArea(5, 10));
 
 // Função para ver data atual
 const dataAtual = () => {
