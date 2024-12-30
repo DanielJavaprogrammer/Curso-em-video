7d74d2691985730eb59574b9a11d2dc6873f35e7
Daniel Sodré <130774958+DanielJavaprogrammer@users.noreply.github.com>
2024-12-30 00:57:21 -0300

    ok

---
+++ JavaScript/objetct-orientation/obj1.js
@@ -0,0 +1,13 @@
+const nomes = ['Daniel', 'Lucas', 'Diogo', 'Joana', 'Vitoria', 'Henrique'];
+const funcionarios = [
+    {
+        nome: 'Daniel',
+        idade: 23,
+        cargo: 'Jornalista'
+    },
+    {
+        nome: 'Geovana',
+        idade: 21,
+        cargo: 'Do Job'
+    }
+];
